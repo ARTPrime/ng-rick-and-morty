6741a92dafd42218a00c9598a77e3b583107b911
Guadarrama-Ruby <flare_tackle@hotmail.com>
2025-08-12 17:16:22 -0600

    Adds angular episodes data load

--- src/app/episodes/components/episode-list-component/episode-list.component.ts
+++ src/app/episodes/components/episode-list-component/episode-list.component.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, signal } from '@angular/core';
+import {
+  PagionationInfo,
+  PaginatedResponse,
+} from '../../../models/paginated-response';
+import { Episode } from '../../../models/episodes';
+import { trackByPropertyName } from '../../../functions/track-by-item-id';
+import { EpisodesService } from '../../services/episodes.service';
+import { take } from 'rxjs';
+import { PageEvent } from '@angular/material/paginator';
 @Component({
   selector: 'app-episode-list-component',
   standalone: false,
   templateUrl: './episode-list.component.html',
   styleUrl: './episode-list.component.scss',
 })
-export class EpisodeListComponent {}
+export class EpisodeListComponent implements OnInit {
+  public paginationInfo = signal<PagionationInfo>({
+    count: 0,
+    next: '',
+    prev: '',
+    pages: 0,
+  });
+
+  public episodes = signal<Episode[]>([]);
+  public trackByItemId = (index: number, item: Episode) =>
+    trackByPropertyName(index, item, 'id');
+
+  constructor(private episodesService: EpisodesService) {}
+
+  /**
+   * Inits component data
+   */
+  public ngOnInit(): void {
+    this.episodesService
+      .getEpisodes()
+      .pipe(take(1))
+      .subscribe((response: PaginatedResponse<Episode>) => {
+        this.paginationInfo.set(response.info);
+        this.episodes.set(response.results);
+      });
+  }
+
+  public onPageEvent(pageEvent: PageEvent) {
+    this.episodesService
+      .getEpisodes(pageEvent.pageIndex + 1)
+      .pipe(take(1))
+      .subscribe((response: PaginatedResponse<Episode>) => {
+        this.paginationInfo.set(response.info);
+        this.episodes.set(response.results);
+      });
+  }
+}
