fb7bd258551dac9d5331cbc3ecfff0d212834833
ARTPrime <primaltorub@gmail.com>
2025-08-19 17:33:25 -0600

    feat: Adds detail panel layout and logic

---
+++ src/app/characters/components/character-detail-component/character-detail-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharacterDetailComponent } from './character-detail.component';
+import { SharedModule } from '../../../shared/shared-module';
+
+@NgModule({
+  declarations: [CharacterDetailComponent],
+  imports: [CommonModule, SharedModule],
+})
+export class CharacterDetailModule {}
